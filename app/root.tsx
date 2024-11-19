b37b16940344041062e6673e150244157af29a26
Kelvin Ward <kelvinward1010@gmail.com>
2024-11-19 20:43:06 +0700

    update fix

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body suppressHydrationWarning>
+      <body suppressHydrationWarning={true}>
         {children}
         <ScrollRestoration />
         <Scripts />
